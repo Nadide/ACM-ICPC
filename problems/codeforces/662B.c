614bb15f8682f08b5777fa7b0726d1e36cb57bc1
nadide <nadidepasali96@gmail.com>
2016-07-22 22:45:39 -0500

    Added dfs examp question

---
+++ problems/codeforces/662B.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+char **g;
+
+int main ()
+{
+	int n,m;
+	int a,b,i,j;
+	char c;
+	g = (char*) malloc (sizeof(char*)*n);
+	for (i=0; i<n; i++)
+		g[i] = (char*) malloc (sizeof(char*)*n);	
+
+	scanf ("%d %d", &n, &m);
+	for (i=0; i<n; i++) {
+		scanf ("%d %d %c", &a,&b,&c);
+		g[a][b] = g[b][a] = c;
+	}
+	for (i=0; i<n; i++) {
+		for (j=0; j<n; j++)
+			printf ("%c ", g[i][j]);
+		printf ("\n");
+	}
+
+	return 0;
+}
